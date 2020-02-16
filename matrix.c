f9e204a020c2474b10f36d5345c2f4c10029e259
KingArthurZ3 <arthurzh@umich.edu>
2020-02-16 15:54:28 -0500

    added starter for lqr controller adt

--- matrix.c
+++ matrix.c
@@ -7,3 +7,66 @@
 //
 
 #include "matrix.h"
+
+void matrix_ctor(Matrix* mat, int r, int c) {
+    mat->rows = r;
+    mat->cols = c;
+    
+    /* Allocate memory for matrix in dynamic memory */
+    mat->arr = (double **) malloc(r * sizeof(double *));
+    for (int i=0; i < r; i++) {
+        mat->arr[i] = (double *)malloc(c * sizeof(double));
+    }
+}
+
+void matrix_dtor(Matrix *mat) {
+    /* Free dynamic memory for arr once finished */
+    for (int i = 0; i < mat->rows; ++i) {
+        free(mat->arr[i]);
+    }
+    free(mat->arr);
+}
+
+int matrix_fill_rc(Matrix *mat, double *arr, int arr_sz, int start, char type) {
+    if (type == 'R' && arr_sz > mat->cols) return 0;
+    if (type == 'C' && arr_sz > mat->rows) return 0;
+    
+    if (type == 'R') {
+        for (int i = 0; i < arr_sz; ++i) {
+            mat->arr[start][i] = arr[i];
+        }
+    } else if (type == 'C'){
+        for (int i = 0; i < arr_sz; ++i) {
+            mat->arr[i][start] = arr[i];
+        }
+    } else {
+        return 0;
+    }
+    
+    return 1;
+}
+
+Matrix* matrix_mult(Matrix *mult, Matrix *mcand) {
+    if (mult->cols != mcand->rows) return NULL;
+    
+    /* Initialize return matrix*/
+    Matrix* mat = NULL;
+    matrix_ctor(mat, mult->rows, mcand->cols);
+    
+    /* Compute matrix product*/
+    for (int i = 0; i < mult->rows; ++i) {
+        for (int j = 0; j < mult->cols; ++j) {
+            double running_sum = 0;
+            for (int k = 0; k < mat->rows; ++k) {
+                running_sum += mult->arr[i][k]*mcand->arr[k][i];
+            } // compute sum of row i col j
+            mat->arr[i][j] = running_sum;
+        } // loop through each column
+    } // loop through each row
+    
+    return mat;
+}
+
+
+
+
--- matrix.h
+++ matrix.h
@@ -12,52 +12,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+    TODO: Longterm Goals (in order of priority) :
+        1. Restructure matrix ADT to represent sparse matrices
+            more efficiently
+        2. Optimize matrix by matrix operations
+ */
+
 typedef struct MatrixStruct {
     int rows, cols;
     double **arr;
 } Matrix;
 
-void matrix_ctor(Matrix* mat, int r, int c) {
-    mat->rows = r;
-    mat->cols = c;
-    
-    /* Allocate memory for matrix in dynamic memory */
-    mat->arr = (double **) malloc(r * sizeof(double *));
-    for (int i=0; i < r; i++) {
-        mat->arr[i] = (double *)malloc(c * sizeof(double));
-    }
+void matrix_ctor(Matrix* mat, int r, int c);
+
+void matrix_dtor(Matrix *mat);
+
+/*
+ TODO: Multiplies all entries in matrix by scalar, saves result back
+        into original matrix
+ */
+void matrix_scal_mult(Matrix *mat, double scalar);
+
+/* Edits specific entry in matrix with value */
+void matrix_fill(Matrix *mat, int row, int col, double value) {
+    if (row >= mat->rows || col >= mat->cols) return;
+    mat->arr[row][col] = value;
 }
 
-void matrix_dtor(Matrix *mat) {
-    /* Free dynamic memory for arr once finished */
-    for (int i = 0; i < mat->rows; ++i) {
-        free(mat->arr[i]);
-    }
-    free(mat->arr);
-}
+/*
+ TODO: Raises matrix to the exponent and results result in output matrix
+ */
+Matrix* matrix_exp(Matrix *mat);
 
 /*
  Copies entries of arr into row or col of matrix, denoted by type parameter
  Setting type = 'R' fills row and type = 'C' fills col
  */
-int matrix_fill_rc(Matrix *mat, double *arr, int arr_sz, int start, char type) {
-    if (type == 'R' && arr_sz > mat->cols) return 0;
-    if (type == 'C' && arr_sz > mat->rows) return 0;
-    
-    if (type == 'R') {
-        for (int i = 0; i < arr_sz; ++i) {
-            mat->arr[start][i] = arr[i];
-        }
-    } else if (type == 'C'){
-        for (int i = 0; i < arr_sz; ++i) {
-            mat->arr[i][start] = arr[i];
-        }
-    } else {
-        return 0;
-    }
-    
-    return 1;
-}
+int matrix_fill_rc(Matrix *mat, double *arr, int arr_sz, int start, char type);
 
 /*
  Multiplies matrix mult and matrix mcand together
@@ -65,47 +57,27 @@ int matrix_fill_rc(Matrix *mat, double *arr, int arr_sz, int start, char type) {
  
  Returns NULL on invalid matrix sizes
  */
-Matrix* matrix_mult(Matrix *mult, Matrix *mcand) {
-    if (mult->cols != mcand->rows) return NULL;
-    
-    /* Initialize return matrix*/
-    Matrix* mat = NULL;
-    matrix_ctor(mat, mult->rows, mcand->cols);
-    
-    /* Compute matrix product*/
-    for (int i = 0; i < mult->rows; ++i) {
-        for (int j = 0; j < mult->cols; ++j) {
-            double running_sum = 0;
-            for (int k = 0; k < mat->rows; ++k) {
-                running_sum += mult->arr[i][k]*mcand->arr[k][i];
-            } // compute sum of row i col j
-            mat->arr[i][j] = running_sum;
-        } // loop through each column
-    } // loop through each row
-    
-    return mat;
-}
+Matrix* matrix_mult(Matrix *mult, Matrix *mcand);
+
+/*
+ TODO: Adds two matrices
+ */
+Matrix* matrix_add(Matrix *lhs, Matrix*rhs);
+
+/*
+ TODO: Subtracts two matrices
+ */
+Matrix* matrix_sub(Matrix *lhs, Matrix*rhs);
+
+/*
+    TODO: Inverts matrix and returns result as new matrix
+ */
+Matrix* matrix_inv(Matrix *mat);
 
 /*
  Copies entries in matrix cpy into matrix mat beginning from start, end
  Returns:   1 for a successful copy and 0 for copying error
  */
-int matrix_fill(Matrix *mat, Matrix*cpy, int start, int end) {
-    int start_row = start;
-    int start_col = end;
-    for (int i = 0; i < cpy->rows; ++i) {
-        if (start_row + i >= mat->rows) {
-            return 0;
-        } // check within row bounds
-        for (int j = 0; j < cpy->cols; ++j) {
-            if (start_col + j >= mat->cols) {
-                return 0;
-            } // check within col bounds
-            mat->arr[start_row+i][start_col+j] = cpy->arr[i][j];
-        }
-    } // copies matrix entries from cpy into mat
-    
-    return 1;
-}
+int matrix_cpy(Matrix *mat, Matrix*cpy, int start, int end);
 
 #endif /* matrix_h */
