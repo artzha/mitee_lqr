cf34cdb704667c735a4cab3dbe73b60bb6406420
KingArthurZ3 <arthurzh@umich.edu>
2020-02-23 14:05:00 -0500

    added skeleton structure for lqr controller

--- controller.c
+++ controller.c
@@ -9,44 +9,49 @@
 #include "controller.h"
 
 void initializeController(Controller *cntl) {
-    /* J constants taken from controller */
-    
-    /* Initialize all matrices to be used in detumbling procedure */
-    matrix_ctor(&cntl->B_b, 3, 3);  // [~B_b] matrix
-    matrix_ctor(&cntl->A_c, 6, 6);  // Ac matrix
-    matrix_ctor(&cntl->B_c, 6, 3);  // Bc matrix
-    matrix_ctor(&cntl->A_d, 6, 6);  // Ad matrix
-    matrix_ctor(&cntl->B_d, 6, 3);  // Bd matrix
-    matrix_ctor(&cntl->I_6 , 6, 6); // 6x6 identity matrix
-    // TODO: Initialize identity matrix properly
-        
-    computeConstants(cntl);
-    computeDynamicInputs(cntl, 0); // assume start at time 0
-}
-
-void computeConstants(Controller *cntl) {
     /* [kg*m^2] Approx for MiTEE 2 (From Three_MT main.m) */
     cntl->J12   = -0.7724;
     cntl->J13   = -0.0463;
     cntl->J23   = 0.7904;
     
-    /* TODO: Ask about what to initilize Bb as */
-    cntl->B_b_prev[0] = -3.211*0.000001;
-    cntl->B_b_prev[1] = -2.378*0.00001;
-    cntl->B_b_prev[2] = -2.026*0.000001;
-        
-    /* Discretize matrix A_c */
-    matrix_scal_mult(&cntl->A_c, TIMESTEP);
-    cntl->A_d = matrix_exp(&cntl->A_c);
+    /* Initialize A_c matrix */
+    
+    /* Initialize A_d matrix */
+    
+    /* Initialize timestep */
+    
+    /* Initialize Q (position) and R (inputs) cost matrices */
+    
+    /* Initialize initial position */
+    
 }
 
 void computeDynamicInputs(Controller *cntl, int time) {
-    /* Discretize matrix B_c */
+    // TODO: Figure out how to compute x position
     
-    // Step 1: Compute -A^-1
-    Matrix *A_c_inv = matrix_inv(&cntl->A_c);
-    matrix_scal_mult(A_c_inv, -1);
-    // Step 2: Compute (I_6 - A_d)
-    Matrix *I_sub_A_d = matrix_sub(&cntl->I_6, cntl->A_d);
-    // Step 3: Compute B_c at time t
+    /* Poll magnetometers for magnetic field readings */
+    
+    /* Recompute B_c matrix */
+    
+    /* Recompute B_d matrix */
+    
+    /* Update satellite position using sensor measurements */
+    
+    /* Compute P(t) matrix using recomputed matrices */
+    computePMatrix();
+    
+    /* Compute K(t) gain matrix using P(t) */
+    
 }
+
+void computePMatrix(/* Inputs */) {
+    // Calls helper function for numerically computing P(t)
+}
+
+void sendMTInputs(/* Inputs */) {
+    /* Compute new inputs to magnetorquers using gain matrix K */
+    
+    /* Send inputs to magnetorquers */
+    
+}
+
--- controller.h
+++ controller.h
@@ -10,7 +10,6 @@
 #define controller_h
 
 #include <stdio.h>
-#include "matrix.h"
 
 // timestep to used for descretized detumbling procedure
 #define TIMESTEP    4
@@ -19,28 +18,34 @@ typedef struct ControllerState {
     int dt;
     double J12, J23, J13;
     double B_b_prev[3];
-    Matrix B_b;
-    Matrix I_6; // 6x6 identity matrix
-    Matrix A_c, B_c;
-    Matrix *A_d, *B_d;
+//    Matrix B_b;
+//    Matrix I_6; // 6x6 identity matrix
+//    Matrix A_c, B_c;
+//    Matrix *A_d, *B_d;
     
 } Controller;
 
 typedef struct SensorState {
     double mag_x, mag_y, mag_z;
     double gyro_x, gyro_y, gyro_z;
-    
-    
 } Sensors;
 
 /* Declares all matrices and constants needed for LQR controller*/
 void initializeController(Controller *cntl);
 
-/* Assigns constants computed in MATLAB/looked up to LQR controller*/
-void computeConstants(Controller *cntl);
-
 /* Computes values for numbers as discrete time steps */
 void computeDynamicInputs(Controller *cntl, int time);
-    
+
+/* Runs Newton Rhapson process to compute P(t) matrix*/
+void computePMatrix(/* Inputs */);
+
+/* Helper function for compute P(t) */
+void runNewtonRhapsonProcess(/* Inputs*/);
+
+/* Computes gain matrix K for magnetorquers*/
+void computeGainMatrix(/* Inputs */);
+
+/* Sends optimal inputs to magnetorquers for stabilization procedure */
+void sendMTInputs(/* Inputs */);
 
 #endif /* controller_h */
